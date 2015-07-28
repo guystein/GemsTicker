9f8ad7eddcfc55d1359692b763d5ca06791bdfdf
Guy S <guys@MacBook-Pro.local>
2015-07-28 21:13:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
 var express = require('express');
 var app = express();
+var request = require("request");
+var q = require('q');
+var moment = require('moment');
+var fs = require('fs');
+var $ = require('jquery');
 
-app.set('port', (process.env.PORT || 5000));
+function getPrice() {
+    var deferred = q.defer();
 
-app.use(express.static(__dirname + '/public'));
+    var url = "http://coinmarketcap-nexuist.rhcloud.com/api/gemz";
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            deferred.resolve(body.price.usd);
+        } else {
+            deferred.reject(error);
+        }
+    });
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+    return deferred.promise;
+}
+
+function getTemplate() {
+    var deferred = q.defer();
+
+    fs.readFile('views/main.html', 'utf8', function (err,data) {
+        if (err) {
+            deferred.reject(error);
+        } else {
+            deferred.resolve(data);
+        }
+    });
+
+    return deferred.promise;
+}
+
+//app.use(express.static('images'));
+app.use('/public', express.static('public'));
+
+
+app.get('/', function (req, res) {
+
+    q.all([getPrice(), getTemplate()]).then(function (output) {
+
+        var html = output[1],
+            price = parseFloat(output[0] * 100, 2).toFixed(2),
+            now = new Date();
+
+        var minutes = "" + now.getMinutes();
+        minutes = minutes.length == 1 ? "0" + minutes : minutes;
+        var dateText = now.getHours() + ":" + minutes;
+
+        html = html.replace('$$$', price + '$');
+        html = html.replace('00:00', dateText);
+
+        //$(html)
+        //    .find('.dollars')
+        //    .text(price + "$")
+        //    .end()
+        //    .find('.time')
+        //    .text('20:45');
+
+        res.send(html);
+
+        //res.send(output[0]);
+
+        //res.send(html);
+
+    });
+
+
+
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
